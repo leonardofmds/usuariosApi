810b01728fb248da12032e6b7460ac4d50a516ec
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-13 13:07:40 -0300

    Querys de Usuario e Perfil

--- src/main/java/br/com/coti/usuariosApi/repositories/PerfilRepository.java
+++ src/main/java/br/com/coti/usuariosApi/repositories/PerfilRepository.java
@@ -2,6 +2,8 @@ package br.com.coti.usuariosApi.repositories;
 
 import br.com.coti.usuariosApi.entities.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,7 +11,8 @@ import java.util.UUID;
 @Repository
 public interface PerfilRepository extends JpaRepository<Perfil, UUID> {
 
-
+    @Query("select p from Perfil p where p.nome = :nome")
+    Perfil findByNome(@Param("nome") String nome);
 
 
 }
--- src/main/java/br/com/coti/usuariosApi/repositories/UsuarioRepository.java
+++ src/main/java/br/com/coti/usuariosApi/repositories/UsuarioRepository.java
@@ -2,10 +2,18 @@ package br.com.coti.usuariosApi.repositories;
 
 import br.com.coti.usuariosApi.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    @Query("select u from Usuario u where u.email = :email")
+    Usuario findbyEmail(@Param("email") String email);
+
+    @Query("select u from Usuario u where u.email = :email and u.senha = :senha")
+    Usuario findByEmailAndSenha(@Param("email") String email, @Param("senha") String senha);
 }
