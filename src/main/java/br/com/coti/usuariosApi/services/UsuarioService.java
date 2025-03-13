994d288f42deb75cb10ee5be6e973676ac2814b6
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-13 13:12:40 -0300

    Create UsuarioService.java

---
+++ src/main/java/br/com/coti/usuariosApi/services/UsuarioService.java
@@ -0,0 +1,42 @@
+package br.com.coti.usuariosApi.services;
+
+import br.com.coti.usuariosApi.components.SHA256Component;
+import br.com.coti.usuariosApi.dtos.CriarUsuarioRequestDto;
+import br.com.coti.usuariosApi.dtos.CriarUsuarioResponseDto;
+import br.com.coti.usuariosApi.entities.Usuario;
+import br.com.coti.usuariosApi.repositories.PerfilRepository;
+import br.com.coti.usuariosApi.repositories.UsuarioRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class UsuarioService {
+
+    @Autowired UsuarioRepository usuarioRepository;
+    @Autowired PerfilRepository perfilRepository;
+    @Autowired SHA256Component sha256Component;
+
+
+    public CriarUsuarioResponseDto criarUsuario(CriarUsuarioRequestDto request){
+
+        if(usuarioRepository.findbyEmail(request.getEmail())!=null){
+            throw new IllegalArgumentException("O email informado já encontra-se cadastrado.");
+        }
+
+        var usuario = new Usuario();
+        BeanUtils.copyProperties(request, usuario);
+        usuario.setPerfil(perfilRepository.findByNome("OPERADOR"));
+        usuario.setSenha(sha256Component.encrypt(request.getSenha()));
+
+        var response = new CriarUsuarioResponseDto();
+        BeanUtils.copyProperties(usuarioRepository.save((usuario)), response);
+        response.setDataCriacao(Instant.now());
+        response.setPerfil(usuario.getPerfil().getNome());
+
+        return response;
+    }
+}
