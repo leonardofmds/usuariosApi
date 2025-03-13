83203e821078645dba8609b41db6f11d4c8bbfc9
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-13 13:09:28 -0300

    Update UsuarioController.java

--- src/main/java/br/com/coti/usuariosApi/controllers/UsuarioController.java
+++ src/main/java/br/com/coti/usuariosApi/controllers/UsuarioController.java
@@ -1,8 +1,15 @@
 package br.com.coti.usuariosApi.controllers;
 
+import br.com.coti.usuariosApi.dtos.CriarUsuarioRequestDto;
+import br.com.coti.usuariosApi.dtos.CriarUsuarioResponseDto;
+import br.com.coti.usuariosApi.services.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/usuario")
 public class UsuarioController {
 
+    @Autowired
+    UsuarioService usuarioService;
+
     @Operation(summary = "Endpoint para cadastrar um novo usuário")
-    @PostMapping("/cadastrar")
-    public void criar(){
-        //TODO
+    @PostMapping("/criar")
+    public ResponseEntity<CriarUsuarioResponseDto> criar(@RequestBody @Valid CriarUsuarioRequestDto request){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.criarUsuario(request));
     }
 
     @Operation(summary = "Endpoint para autenticar um usuário")
