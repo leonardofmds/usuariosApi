3480c3a1bae9ca1f5af4c31278415410c29722bf
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-13 13:14:18 -0300

    Create IllegalArgumentExceptionHandler.java
    Handler para retornar um http 400 bad request invés de um 500 ao fazer validações de dados já existentes, como exemplo email já cadastrado no criarUsuario em UsuarioService

---
+++ src/main/java/br/com/coti/usuariosApi/handlers/IllegalArgumentExceptionHandler.java
@@ -0,0 +1,15 @@
+package br.com.coti.usuariosApi.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class IllegalArgumentExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
