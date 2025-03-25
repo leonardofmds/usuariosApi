72ef19fe935e02815fa608ee6592df53bf092615
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-25 09:29:46 -0300

    Create JwtTokenComponent.java

---
+++ src/main/java/br/com/coti/usuariosApi/components/JwtTokenComponent.java
@@ -0,0 +1,28 @@
+package br.com.coti.usuariosApi.components;
+
+import br.com.coti.usuariosApi.entities.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenComponent {
+    /*
+     * Método para gerar o token JWT com os dados do usuário autenticado
+     */
+    public String getToken(Usuario usuario) {
+
+        //chave para criptografia do TOKEN: cce09a14-97f4-409e-bbed-182d4e1c3002
+        var secretKey = "cce09a14-97f4-409e-bbed-182d4e1c3002";
+
+        return Jwts.builder()
+                .setSubject(usuario.getEmail()) //email do usuário
+                .claim("perfil", usuario.getPerfil().getNome()) //perfil do usuário
+                .setIssuedAt(new Date())  //data de geração do token
+                .setExpiration(new Date(System.currentTimeMillis() + 1800000)) //data de expiração do token
+                .signWith(SignatureAlgorithm.HS256, secretKey) //chave para assinatura do token
+                .compact();
+    }
+}
