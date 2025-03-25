def59a8cd253abf6b68194f5f83fd554ff12afa5
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-25 09:29:20 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/coti/usuariosApi/repositories/UsuarioRepository.java
+++ src/main/java/br/com/coti/usuariosApi/repositories/UsuarioRepository.java
@@ -14,6 +14,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
     @Query("select u from Usuario u where u.email = :email")
     Usuario findbyEmail(@Param("email") String email);
 
-    @Query("select u from Usuario u where u.email = :email and u.senha = :senha")
+    @Query("select u from Usuario u join u.perfil p where u.email = :email and u.senha = :senha")
     Usuario findByEmailAndSenha(@Param("email") String email, @Param("senha") String senha);
 }
